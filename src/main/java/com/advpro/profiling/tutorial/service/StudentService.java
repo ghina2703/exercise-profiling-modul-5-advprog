193df640d1da8977bdeae8da8ca57b79f4ee539f
ghina2703 <ghina2703@gmail.com>
2025-03-12 15:28:01 +0700

    [Refactoring] Optimizing method getAllStudentsWithCourses performance

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -7,10 +7,13 @@ import com.advpro.profiling.tutorial.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
+/**
+ * @author muhammad.khadafi
+ */
 @Service
 public class StudentService {
 
@@ -25,15 +28,25 @@ public class StudentService {
     }
 
     public Optional<Student> findStudentWithHighestGpa() {
-        return studentRepository.findAll()
-                .stream()
-                .max((s1, s2) -> Double.compare(s1.getGpa(), s2.getGpa()));
+        List<Student> students = studentRepository.findAll();
+        Student highestGpaStudent = null;
+        double highestGpa = 0.0;
+        for (Student student : students) {
+            if (student.getGpa() > highestGpa) {
+                highestGpa = student.getGpa();
+                highestGpaStudent = student;
+            }
+        }
+        return Optional.ofNullable(highestGpaStudent);
     }
 
     public String joinStudentNames() {
-        return studentRepository.findAll()
-                .stream()
-                .map(Student::getName)
-                .collect(Collectors.joining(", "));
+        List<Student> students = studentRepository.findAll();
+        String result = "";
+        for (Student student : students) {
+            result += student.getName() + ", ";
+        }
+        return result.substring(0, result.length() - 2);
     }
 }
+
